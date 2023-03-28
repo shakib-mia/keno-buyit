4b3c208bdb02c942bc113c7ca9505d08b9974693
Md. Shakib Mia <smdshakibmia2001@gmail.com>
2023-03-28 10:33:11 +0600

    tailwindcss added

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './../../assets/logo.png'
+
+const Navbar = () => {
+    return (
+        <div className='navbar'>
+            <img src={logo} alt="" />
+            <ul>
+                <li>Home</li>
+                <li>Home</li>
+                <li>Home</li>
+                <li>Home</li>
+                <li>Home</li>
+            </ul>
+        </div>
+    );
+};
+
+export default Navbar;
